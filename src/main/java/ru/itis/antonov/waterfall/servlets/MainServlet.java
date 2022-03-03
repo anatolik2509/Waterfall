0cda95188bac20d1c550f31dd65bd143c3f18ad8
anatolik2509 <antonov250901@mail.ru>
2022-03-03 18:43:29 +0300

    funny comment

--- src/main/java/ru/itis/antonov/waterfall/servlets/MainServlet.java
+++ src/main/java/ru/itis/antonov/waterfall/servlets/MainServlet.java
@@ -7,12 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
+
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.sendRedirect("/feed");
-        //hello
-        //aaabbb
     }
 }
